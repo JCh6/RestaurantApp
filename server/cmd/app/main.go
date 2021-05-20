8425ac43f47c73a1139398ee90d555432a051796
Juan Chaparro <juanchaparro@192.168.1.113>
2021-05-19 21:18:04 -0500

    testing dgraph

--- server/cmd/app/main.go
+++ server/cmd/app/main.go
@@ -1,18 +1,184 @@
 package main
 
 import (
-	"restaurantapp/cmd/handler"
+	/*"restaurantapp/cmd/handler"
 	"restaurantapp/pkg/models/products"
 	"github.com/go-chi/chi/v5"
-	"net/http"
+	"net/http"*/
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+	"google.golang.org/grpc"
+	"github.com/dgraph-io/dgo/v200"
+	"github.com/dgraph-io/dgo/v200/protos/api"
 )
 
+type School struct {
+	Name  string   `json:"name,omitempty"`
+	DType []string `json:"dgraph.type,omitempty"`
+}
+
+type loc struct {
+	Type   string    `json:"type,omitempty"`
+	Coords []float64 `json:"coordinates,omitempty"`
+}
+
+// If omitempty is not set, then edges with empty values (0 for int/float, "" for string, false
+// for bool) would be created for values not specified explicitly.
+
+type Person struct {
+	Uid      string     `json:"uid,omitempty"`
+	Name     string     `json:"name,omitempty"`
+	Age      int        `json:"age,omitempty"`
+	Dob      *time.Time `json:"dob,omitempty"`
+	Married  bool       `json:"married,omitempty"`
+	Raw      []byte     `json:"raw_bytes,omitempty"`
+	Friends  []Person   `json:"friend,omitempty"`
+	Location loc        `json:"loc,omitempty"`
+	School   []School   `json:"school,omitempty"`
+	DType    []string   `json:"dgraph.type,omitempty"`
+}
+
 func main () {
-	port := ":3000"
+	/*port := ":9080"
 	products := products.New()
-	r := chi.NewRouter()
+	rr := chi.NewRouter()
 
-	r.Get("/products", handler.ProductsGet(products))
+	rr.Get("/products", handler.ProductsGet(products))
 
-	http.ListenAndServe(port, r)
+	http.ListenAndServe(port, rr)*/
+	///////////////////
+
+	dg := getDgraphClient()
+	dob := time.Date(1980, 01, 01, 23, 0, 0, 0, time.UTC)
+	log.Println(dob)
+
+	p := Person{
+		Uid:     "_:alice",
+		Name:    "Alice",
+		Age:     26,
+		Married: true,
+		DType:   []string{"Person"},
+		Location: loc{
+			Type:   "Point",
+			Coords: []float64{1.1, 2},
+		},
+		Dob: &dob,
+		Raw: []byte("raw_bytes"),
+		Friends: []Person{{
+			Name:  "Bob",
+			Age:   24,
+			DType: []string{"Person"},
+		}, {
+			Name:  "Charlie",
+			Age:   29,
+			DType: []string{"Person"},
+		}},
+		School: []School{{
+			Name:  "Crown Public School",
+			DType: []string{"Institution"},
+		}},
+	}
+
+	op := &api.Operation{}
+	op.Schema = `
+		name: string @index(exact) .
+		age: int .
+		married: bool .
+		loc: geo .
+		dob: datetime .
+		Friend: [uid] .
+		type: string .
+		coords: float .
+		type Person {
+			name: string
+			age: int
+			married: bool
+			Friend: [Person]
+			loc: Loc
+		}
+		type Institution {
+			name: string
+		}
+		type Loc {
+			type: string
+			coords: float
+		}
+	`
+
+	ctx := context.Background()
+	if err := dg.Alter(ctx, op); err != nil {
+		log.Fatal(err)
+	}
+
+	mu := &api.Mutation{
+		CommitNow: true,
+	}
+	pb, err := json.Marshal(p)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mu.SetJson = pb
+	response, err := dg.NewTxn().Mutate(ctx, mu)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Assigned uids for nodes which were created would be returned in the response.Uids map.
+	variables := map[string]string{"$id1": response.Uids["alice"]}
+	q := `query Me($id1: string){
+		me(func: uid($id1)) {
+			name
+			dob
+			age
+			loc
+			raw_bytes
+			married
+			dgraph.type
+			friend @filter(eq(name, "Bob")){
+				name
+				age
+				dgraph.type
+			}
+			school {
+				name
+				dgraph.type
+			}
+		}
+	}`
+
+	resp, err := dg.NewTxn().QueryWithVars(ctx, q, variables)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	type Root struct {
+		Me []Person `json:"me"`
+	}
+
+	var r Root
+	err = json.Unmarshal(resp.Json, &r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	out, _ := json.MarshalIndent(r, "", "\t")
+	fmt.Printf("%s\n", out)
+
+}
+
+func getDgraphClient() *dgo.Dgraph {
+	// Dial a gRPC connection. The address to dial to can be configured when
+	// setting up the dgraph cluster.
+	d, err := grpc.Dial("localhost:9080", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return dgo.NewDgraphClient(
+		api.NewDgraphClient(d),
+	)
 }
